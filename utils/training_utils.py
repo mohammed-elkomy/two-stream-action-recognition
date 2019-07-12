7e3068d9efdb27b603188c7605719c41fb5864c7
Mohammed Alaa El komy <mohammed.a.elkomy@gmail.com>
2019-07-12 13:00:21 +0200

    Update training_utils.py

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -19,22 +19,22 @@ def get_validation_callback(log_stream, validate_every, model, test_loader, test
     """
 
     class ValidationCallback(tf.keras.callbacks.Callback):
-        def on_batch_end(self, batch, logs={}):
-
-            metrics_log = ''
-            for k in self.params['metrics']:
-                if k in logs:
-                    val = logs[k]
-                    if abs(val) > 1e-3:
-                        metrics_log += ' - %s: %.4f' % (k, val)
-                    else:
-                        metrics_log += ' - %s: %.4e' % (k, val)
-            print('{} ... {}'.format(
-                                        self.params['samples'],
-                                        metrics_log))
-
-            print(batch)
-            print("="*50)
+        # def on_batch_end(self, batch, logs={}):
+        # 
+        #     metrics_log = ''
+        #     for k in self.params['metrics']:
+        #         if k in logs:
+        #             val = logs[k]
+        #             if abs(val) > 1e-3:
+        #                 metrics_log += ' - %s: %.4f' % (k, val)
+        #             else:
+        #                 metrics_log += ' - %s: %.4e' % (k, val)
+        #     print('{} ... {}'.format(
+        #                                 self.params['samples'],
+        #                                 metrics_log))
+        # 
+        #     print(batch)
+        #     print("="*50)
 
         def on_epoch_end(self, epoch, logs=None):
             """
